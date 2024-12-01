0ed04bb1996790fe7e03c05368f7a884f1d37c21
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-12-01 18:47:07 +0200

    Added CartScreen UI & Render Data From Cart State & Added Functionalities For incrementCartItemQuantity, decrementCartItemQuantity For CartScreen

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,15 +1,137 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import { useStore } from "../../store/";
+import { COLORS, SPACING } from "../../theme/theme";
+import HeaderBar from "../../components/HeaderBar";
+import EmptyListAnimation from "../../components/EmptyListAnimation";
+import PaymentFooter from "../../components/PaymentFooter";
+import { router } from "expo-router";
+import CartItem from "../../components/CartItem";
 
 const CartScreen = () => {
+  const CartList = useStore((state: any) => state.CartList);
+  const CartPrice = useStore((state: any) => state.CartPrice);
+  const incrementCartItemQuantity = useStore(
+    (state: any) => state.incrementCartItemQuantity
+  );
+  const decrementCartItemQuantity = useStore(
+    (state: any) => state.decrementCartItemQuantity
+  );
+  const calculateCartPrice = useStore((state: any) => state.calculateCartPrice);
+
+  const buttonPressHandler = () => {
+    router.push({
+      pathname: "/payment",
+      params: {
+        amount: CartPrice,
+      },
+    });
+  };
+
+  const incrementCartItemQuantityHandler = (id: string, size: string) => {
+    incrementCartItemQuantity(id, size);
+    calculateCartPrice();
+  };
+
+  const decrementCartItemQuantityHandler = (id: string, size: string) => {
+    decrementCartItemQuantity(id, size);
+    calculateCartPrice();
+  };
+
   return (
-    <SafeAreaView>
-      <Text>CartScreen</Text>
-    </SafeAreaView>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}
+      >
+        <View style={styles.ScrollViewInnerView}>
+          <View style={styles.ItemContainer}>
+            <HeaderBar title="Cart" />
+
+            {CartList.length == 0 ? (
+              <EmptyListAnimation title={"Cart is Empty"} />
+            ) : (
+              <View style={styles.ListItemContainer}>
+                {CartList.map((data: any) => (
+                  <TouchableOpacity
+                    onPress={() => {
+                      router.push({
+                        pathname: "/details",
+                        params: {
+                          index: data.index.toString(), // Convert to string to ensure proper parsing
+                          id: data.id,
+                          type: data.type,
+                        },
+                      });
+                    }}
+                    key={data.id}
+                  >
+                    <CartItem
+                      id={data.id}
+                      name={data.name}
+                      imagelink_square={data.imagelink_square}
+                      special_ingredient={data.special_ingredient}
+                      roasted={data.roasted}
+                      prices={data.prices}
+                      type={data.type}
+                      incrementCartItemQuantityHandler={
+                        incrementCartItemQuantityHandler
+                      }
+                      decrementCartItemQuantityHandler={
+                        decrementCartItemQuantityHandler
+                      }
+                    />
+                  </TouchableOpacity>
+                ))}
+              </View>
+            )}
+          </View>
+
+          {CartList.length != 0 ? (
+            <PaymentFooter
+              buttonPressHandler={buttonPressHandler}
+              buttonTitle="Pay"
+              price={{ price: CartPrice, currency: "$" }}
+            />
+          ) : (
+            <></>
+          )}
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
-export default CartScreen;
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+    paddingTop: 40,
+    paddingBottom: 100,
+  },
+  ScrollViewFlex: {
+    flexGrow: 1,
+  },
+  ScrollViewInnerView: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  ItemContainer: {
+    flex: 1,
+  },
+  ListItemContainer: {
+    paddingHorizontal: SPACING.space_20,
+    gap: SPACING.space_20,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default CartScreen;
